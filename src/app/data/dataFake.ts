d91b13572dc57307b83559418cbc070eac5fb807
juniorpgm34 <pitagorasmattos@gmail.com>
2023-05-04 15:33:55 -0300

    up

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -2,43 +2,75 @@ export const dataFake = [
     {
         'id':'1',
         'title': 'NOVO HOMEM DE FERRO EM 3D',
-        'description': 'Marvel anúncia novo homem de ferro',
+        'description': `Em uma coletiva de imprensa realizada hoje, a Marvel Studios revelou seu mais novo projeto: um filme do Homem de Ferro em 3D. 
+        \n A grande novidade é que o personagem principal será criado inteiramente em computação gráfica, permitindo que os produtores criem cenas de ação espetaculares e realistas que seriam impossíveis de filmar com atores reais. 
+        \nO novo Homem de Ferro será dublado por ninguém menos que Robert Downey Jr., que interpretou o personagem nos filmes anteriores da Marvel. "Estou muito animado em fazer parte deste projeto inovador e trazer o Homem de Ferro de uma maneira totalmente nova para os fãs", disse o ator.  
+        \n O processo de criação do Homem de Ferro em 3D é extremamente complexo, envolvendo a captura de movimentos e expressões faciais de Downey Jr. através de uma série de câmeras especiais. Esses dados são então processados por computadores de última geração para criar uma versão digital extremamente detalhada do ator, que será a base para a animação do personagem.`,
         'photoCover': 'https://media.contentapi.ea.com/content/dam/news/www-ea/images/2022/09/ea-motive-new-title-teaser-16x9-featured.jpg.adapt.crop191x100.628p.jpg'
     },
     {
         'id':'2',
         'title': 'NOVO JEDI NA ÁREA',
-        'description': 'ola',
+        'description': ` Há rumores de que uma nova figura apareceu no universo de Star Wars, e ela está causando um alvoroço entre os fãs da saga. De acordo com fontes confiáveis, Mestre Yoda apareceu em uma aparição astral para a família Skywalker, alertando-os sobre um novo Jedi que está abalando as estruturas da galáxia.
+
+        Dizem que este novo Jedi é tão poderoso que é capaz de rivalizar com os mais habilidosos Cavaleiros Jedi, mas sua verdadeira identidade permanece um mistério. Alguns dizem que ele é um ser de outra galáxia, enquanto outros afirmam que ele é simplesmente um fanático por Star Wars que se fantasiou de Jedi.
+        
+        Independentemente de sua verdadeira origem, a notícia rapidamente se espalhou pelas redes sociais, com fãs ansiosos tentando descobrir mais informações sobre esse Jedi misterioso. Alguns até mesmo criaram memes hilários, retratando o novo Jedi como um ser extraterrestre com habilidades absurdas.
+        
+        Enquanto isso, os verdadeiros fãs de Star Wars permanecem céticos, lembrando que existem muitos charlatões por aí que tentam se passar por Jedi. Será que este novo Jedi é realmente tão poderoso quanto Mestre Yoda afirma? Só o tempo dirá. `,
         'photoCover': 'https://uploads.jovemnerd.com.br/wp-content/uploads/2023/04/star_wars_jedi_survivor_review__3h7u25g-1210x544.jpg'
     },
     {
         'id':'3',
         'title': 'Nova série anúnciada',
-        'description': 'ola',
+        'description': 'Neste domingo, 15, a partir das 23h, a HBO estreia adaptação de um dos jogos mais amados e premiados da história. The Last of Us, estrelado por Pedro Pascal (Joey) e Bella Ramsay (Ellie), estreia no streaming e nos canais do estúdio. A série, que traz a história de sobrevivência da humanidade após um apocalipse, vem de um extenso trabalho de Bruce Straley e Neil Druckmann, diretores de criação. Anos depois da liberação dos jogos e suas expansões, a adaptação ao audiovisual não só é muito aguardada pelos fãs, como também uma nova aposta para adaptações de videogames. ',
         'photoCover': 'https://cdn.diariodegoias.com.br/wp-content/uploads/2022/12/the-last-of-us-serie-hbo-jogo-estreia-lancamento.jpg'
     },
     {
         'id':'4',
         'title': 'Capitão América morreu ?',
-        'description': 'ola',
+        'description': `
+        O Capitão América é um dos heróis mais icônicos da Marvel Comics, conhecido por sua bravura, lealdade e senso de justiça. Porém, há rumores de que o herói possa encontrar seu fim em um futuro próximo.
+        
+        De acordo com fontes próximas aos estúdios da Marvel, o personagem poderá morrer em uma das próximas produções do universo cinematográfico da Marvel. Embora ainda não haja confirmação oficial, os fãs estão preocupados com a possibilidade de perder um dos seus heróis favoritos.
+        
+        Caso essa notícia se confirme, o impacto na comunidade dos fãs da Marvel seria imenso. O Capitão América tem sido um dos personagens mais queridos e admirados pelos fãs ao longo dos anos, e sua morte seria uma perda irreparável para muitos.
+        
+        No entanto, a Marvel é conhecida por sua habilidade em contar histórias emocionantes e surpreendentes, e a morte do Capitão América poderia ser uma oportunidade para explorar novos temas e personagens no universo dos super-heróis.
+        
+        Até que haja uma confirmação oficial sobre o destino do Capitão América, os fãs continuarão ansiosos e especulando sobre o futuro do herói e do universo Marvel como um todo. `,
         'photoCover': 'https://www.einerd.com.br/wp-content/uploads/2019/04/capit%C3%A3o-am%C3%A9rica-ultimato-e1562254795838.jpg'
     },
     {
         'id':'5',
         'title': 'Lacoste faz ponta em filme',
-        'description': 'ola',
+        'description': ` Lacoste, o personagem misterioso que tem gerado curiosidade entre os fãs da Marvel, finalmente fez sua tão esperada aparição em um dos filmes mais aguardados do ano. E acredite, a Marvel Studios desembolsou uma fortuna para que o ator pudesse interpretar o personagem.
+
+        Segundo fontes confiáveis, o ator é conhecido por seu carisma e elegância, o que chamou a atenção da Marvel e lhe rendeu a oportunidade de fazer uma ponta no filme. E o resultado foi surpreendente.
+        
+        Lacoste, em sua breve aparição de 10 minutos, conseguiu ser o auge do filme. Com seu jeito charmoso e carismático, o personagem conseguiu roubar a cena e deixou muitos fãs com um gostinho de "quero mais".
+        
+        Aliás, a participação de Lacoste foi tão marcante que já há especulações de que o personagem possa ganhar um spin-off próprio. Será que veremos mais do nosso jacarezinho preferido em breve?
+        
+        Por enquanto, nos resta esperar e torcer para que a Marvel traga Lacoste de volta para suas futuras produções. Afinal, depois de uma aparição tão brilhante, não podemos ficar sem esse personagem tão cativante.`,
         'photoCover': 'https://br.web.img3.acsta.net/newsv7/21/07/26/18/49/2684291.jpg'
     },
     {
         'id':'6',
         'title': 'O Mago é implacável',
-        'description': 'ola',
+        'description': ` Se você é um fã de fantasia e magia, certamente já ouviu falar de Zoltar, o místico mago que possui incríveis poderes sobrenaturais. Mas você sabia que ele acabou de embarcar em uma nova jornada em busca de um artefato místico perdido há muitos anos?
+
+        Zoltar, o famoso mago que já foi reverenciado em todo o reino por suas habilidades sobrenaturais, acaba de receber uma nova missão. Ele foi chamado para encontrar o "Olho da Serpente", um objeto místico que dizem ter o poder de controlar todos os elementos da natureza.
+        
+        Para os fãs de fantasia, esta nova jornada de Zoltar é uma oportunidade emocionante de seguir as aventuras deste personagem lendário em uma missão incrível. Mas o que torna esta história ainda mais interessante é a possibilidade de explorar temas mais profundos por trás da narrativa.
+        
+        Por exemplo, podemos refletir sobre a coragem de Zoltar em enfrentar tantos desafios para alcançar seu objetivo, e sobre o que isso pode nos ensinar sobre a importância da perseverança e da determinação. Além disso, podemos analisar como o tema do poder é explorado na história, e como ele pode ser utilizado para representar questões mais amplas sobre o controle e a dominação.`,
         'photoCover': 'https://i.pinimg.com/600x315/ae/29/a9/ae29a9ff564575cd69287fefb28c8c0f.jpg'
     },
     {
         'id':'7',
         'title': 'Geração raiz VS geração nutela, quem vence ?',
-        'description': 'ola',
+        'description': ` Em desenvolvimento...`,
         'photoCover': 'https://cinepop.com.br/wp-content/uploads/2023/03/zachary-levi-adao-negro-shazam-dwayne-johnson-the-rock.jpg'
     }
 ]
